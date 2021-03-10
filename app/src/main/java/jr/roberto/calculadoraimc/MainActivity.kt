88286617911a2ed17f26bf43209d5ddde7e8f0ee
Roberto Júnior <rbezerra18@hotmail.com>
2021-03-10 19:37:20 -0300

    Build app Calculadora IMC.

--- app/src/main/java/jr/roberto/calculadoraimc/MainActivity.kt
+++ app/src/main/java/jr/roberto/calculadoraimc/MainActivity.kt
@@ -2,13 +2,46 @@ package jr.roberto.calculadoraimc
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //setListeners()
         Log.w("lifecycle","CREATE. Estou criando a tela!")
+
+        val alturaEDT: EditText = alturaEDT
+        val pesoEDT: EditText = pesoEDT
+        val calcularBTN: Button = calcularBTN
+
+        alturaEDT?.doAfterTextChanged() { _ -> }
+
+        pesoEDT.doOnTextChanged() { _, _, _, _ -> }
+
+        calcularBTN?.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+
+    }
+
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        var subtituloTXT: TextView = subtituloTXT
+        val imc: Float
+
+        if(peso != null && altura != null) {
+            imc = peso / (altura * altura)
+            subtituloTXT.setText("Seu IMC é $imc!")
+        }
+
     }
 
     override fun onStart() {
@@ -40,4 +73,5 @@ class MainActivity : AppCompatActivity() {
         Log.w("lifecycle","DESTROY. A tela foi destruída!")
         super.onDestroy()
     }
+
 }
